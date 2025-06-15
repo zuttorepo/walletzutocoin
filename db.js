f46b40f541e34efcecb1b058b9033a573fe056c4
zuttorepo <walletzuttocoin@gmail.com>
2025-06-15 21:53:59 +0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const level = require('level');
+const db = level('./ztcdb');
+
+module.exports = {
+  put: (key, value) => db.put(key, JSON.stringify(value)),
+  get: async (key) => {
+    try {
+      const value = await db.get(key);
+      return JSON.parse(value);
+    } catch (e) {
+      if (e.notFound) return null;
+      throw e;
+    }
+  },
+  del: (key) => db.del(key),
+  db // export db untuk stream createReadStream
+};
