2b8a94450a2e1fd496ee57c8e85771da4f9b64f8
zuttorepo <walletzuttocoin@gmail.com>
2025-06-16 20:46:27 +0700

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/address', require('./address/balance'));
-router.use('/tx/send', require('./tx/send'));
-router.use('/tx/create', require('./tx/create'));
-router.use('/tx/sign', require('./tx/sign'));
+router.use('/address', require('./address'));
+router.use('/tx', require('./tx')); // kalau ada tx.js
+// Tambahkan routes lain sesuai kebutuhan
 
 module.exports = router;
