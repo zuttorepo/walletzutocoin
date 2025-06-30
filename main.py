68315c47273b5a5734f43fbddc7d973142035eab
zuttorepo <walletzuttocoin@gmail.com>
2025-06-30 09:35:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes import wallet, transaction
+from routes import supply  # Tambahkan import
 
 app = FastAPI()
 app.include_router(wallet.router, prefix="/wallet")
 app.include_router(transaction.router, prefix="/tx")
+app.include_router(supply.router)  # Tambahkan ini
