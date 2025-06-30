2b3e4cdd679492b5e1f9ca2f31fdfea3a279f500
zuttorepo <walletzuttocoin@gmail.com>
2025-06-30 09:47:58 +0700

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,10 @@
+# core/config.py
+
+# Jumlah reward per block Zuttocoin
+REWARD_PER_BLOCK = 50
+
+# RPC default config (opsional kalau ingin dipusatkan di sini juga)
+RPC_HOST = "127.0.0.1"
+RPC_PORT = 8332
+RPC_USER = "ztcuser"
+RPC_PASSWORD = "ztcpass"
